0afb459d1e9ea14e11dec6ab937372d53b75b40d
Rei Terasawa <rei.terasawa0823@gmail.com>
2023-12-09 22:22:31 -0600

    add simple slide animation with jQuery

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 $("button").click(() => {
     $("a.google").attr("href", "https://www.google.com/");
     $("a.google").addClass("red");
+    $("h1").slideToggle();
 })
 
 $("input").keypress(event => {
     // console.log(event.key)
     $("h1").text($("input").val());
-    console.log($("input").val());
+    // console.log($("input").val());
 })
